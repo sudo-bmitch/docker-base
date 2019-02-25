69816acccf9927c9615d0ab4826c4c73fa78ac9e
Brandon Mitchell <git@bmitch.net>
2019-02-24 20:01:59 -0500

    Updating registry and repository variables to better support local environments

--- build-local.sh
+++ build-local.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-REPOSITORY=localhost:5000
+REGISTRY=localhost:5000/bmitch-public
+BASE_REPOSITORY=base
 build_date=$(date +%Y%m%d-%H%M%S)
 vcs_ref="undef"
 vcs_url="undef"
@@ -9,9 +10,10 @@ common_args=""
 common_args="${common_args} --label org.label-schema.build-date=${build_date}"
 common_args="${common_args} --label org.label-schema.vcs-ref=${vcs_ref}"
 common_args="${common_args} --label org.label-schema.vcs-url=${vcs_url}"
-common_args="${common_args} --build-arg REPOSITORY=${REPOSITORY}"
+common_args="${common_args} --build-arg REGISTRY=${REGISTRY}"
+common_args="${common_args} --build-arg BASE_REPOSITORY=${BASE_REPOSITORY}"
 
-docker build ${common_args} -t ${REPOSITORY}/base:debian  -f Dockerfile.debian  .
-docker build ${common_args} -t ${REPOSITORY}/base:alpine  -f Dockerfile.alpine  .
-docker build ${common_args} -t ${REPOSITORY}/base:scratch -f Dockerfile.scratch .
+docker build ${common_args} -t ${REGISTRY}/${BASE_REPOSITORY}:debian  -f Dockerfile.debian  .
+docker build ${common_args} -t ${REGISTRY}/${BASE_REPOSITORY}:alpine  -f Dockerfile.alpine  .
+docker build ${common_args} -t ${REGISTRY}/${BASE_REPOSITORY}:scratch -f Dockerfile.scratch .
 
--- build-public.sh
+++ build-public.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-REPOSITORY=sudobmitch
+REGISTRY=docker.io
+BASE_REPOSITORY=sudobmitch/base
 build_date=$(date +%Y%m%d-%H%M%S)
 vcs_ref="undef"
 vcs_url="https://github.com/sudo-bmitch/base"
@@ -9,9 +10,10 @@ common_args=""
 common_args="${common_args} --label org.label-schema.build-date=${build_date}"
 common_args="${common_args} --label org.label-schema.vcs-ref=${vcs_ref}"
 common_args="${common_args} --label org.label-schema.vcs-url=${vcs_url}"
-common_args="${common_args} --build-arg REPOSITORY=${REPOSITORY}"
+common_args="${common_args} --build-arg REGISTRY=${REGISTRY}"
+common_args="${common_args} --build-arg BASE_REPOSITORY=${BASE_REPOSITORY}"
 
-docker build ${common_args} -t ${REPOSITORY}/base:debian  -f Dockerfile.debian  .
-docker build ${common_args} -t ${REPOSITORY}/base:alpine  -f Dockerfile.alpine  .
-docker build ${common_args} -t ${REPOSITORY}/base:scratch -f Dockerfile.scratch .
+docker build ${common_args} -t ${REGISTRY}/${BASE_REPOSITORY}/base:debian  -f Dockerfile.debian  .
+docker build ${common_args} -t ${REGISTRY}/${BASE_REPOSITORY}/base:alpine  -f Dockerfile.alpine  .
+docker build ${common_args} -t ${REGISTRY}/${BASE_REPOSITORY}/base:scratch -f Dockerfile.scratch .
 
