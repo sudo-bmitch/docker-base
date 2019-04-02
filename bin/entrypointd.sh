f1b7e1a775d41d93b354037f193c2dcc8946ee34
Brandon Mitchell <git@bmitch.net>
2019-04-02 15:45:29 -0400

    Adding volume caching
    Signed-off-by: Brandon Mitchell <git@bmitch.net>

--- bin/entrypointd.sh
+++ bin/entrypointd.sh
@@ -4,6 +4,8 @@
 # License: MIT
 
 set -e
+# Handle a kill signal before the final "exec" command runs
+trap "{ exit 0; }" TERM INT
 
 # strip off "/bin/sh -c" args from a string CMD
 if [ $# -gt 1 ] && [ "$1" = "/bin/sh" ] && [ "$2" = "-c" ]; then
@@ -11,23 +13,29 @@ if [ $# -gt 1 ] && [ "$1" = "/bin/sh" ] && [ "$2" = "-c" ]; then
   eval "set -- $1"
 fi
 
+if [ -f /.volume-cache/volume-list.already-run ]; then
+  rm /.volume-cache/volume-list.already-run
+fi
+
 . secret-vars
 
-# source files ending in ".env"
-for ep in /etc/entrypoint.d/*.env; do
-  if [ -f "${ep}" ]; then
+for ep in /etc/entrypoint.d/*; do
+  ext="${ep##*.}"
+  if [ "${ext}" = "env" -a -f "${ep}" ]; then
+    # source files ending in ".env"
     echo "Sourcing: ${ep}"
     set -a && . "${ep}" && set +a
-  fi
-done
-
-# run scripts ending in ".sh"
-for ep in /etc/entrypoint.d/*.sh; do
-  if [ -x "${ep}" ]; then
+  elif [ "${ext}" = "sh" -a -x "${ep}" ]; then
+    # run scripts ending in ".sh"
     echo "Running: ${ep}"
     "${ep}"
   fi
 done
+
+# load any cached volumes
+if [ -f /.volume-cache/volume-list -a ! -f /.volume-cache/volume-list.already-run ]; then
+  load-volume -a
+fi
 
 # Default to the prior entrypoint if defined
 if [ -n "$ORIG_ENTRYPOINT" ]; then
